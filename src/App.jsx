c50eb86a81f9595eaca97e76ee5e80c1840a4176
Methu Islam <djmithu1999@gmail.com>
2025-03-04 17:54:38 +0600

    react knowledge cafe

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './Components/Header/Header'
+import Blogs from './Components/Blogs/Blogs'
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Blogs />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+      <h1 className='text-4xl text-blod'>react knowledge cafe</h1>
+    </div>
+  )
+}
+
+export default Header
